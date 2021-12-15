6b5cdf5dfebc7c581feabb904f67c3f856085813
Gulnaaznasrin21 <84378699+Gulnaaznasrin21@users.noreply.github.com>
2021-12-15 15:41:49 +0530

    Update hp.js

--- hp.js
+++ hp.js
@@ -3,6 +3,7 @@ const hypotenuseBtn = document.querySelector("#Hypotenuse-btn");
 const outputEl = document.querySelector("#output");
 
 function calculateSumOfSquares(a, b) {
+
     const sumOfSquares = a * a + b * b;
     return sumOfSquares;
 
@@ -10,9 +11,12 @@ function calculateSumOfSquares(a, b) {
 }
 
 function calculateHypotenuse() {
-    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
-    const lengthofHypotenuse = Math.sqrt(sumOfSquares);
-    outputEl.innerText = "The length of Hypotenuse is" + lengthofHypotenuse;
-
+    if(Number(sides[0].value)>0 && Number(sides[1].value)>0){
+        const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+        const lengthofHypotenuse = Math.sqrt(sumOfSquares);
+        outputEl.innerText = "The length of Hypotenuse is" + lengthofHypotenuse;
+    }else{
+        outputEl.innerText="You entered negative numbers enter valid number"
+    }
 }
 hypotenuseBtn.addEventListener("click", calculateHypotenuse);
